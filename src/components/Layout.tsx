7ea8a5c4edb11fd472fd3d39e127295f72ee0b76
ketyung <ketyung@gmail.com>
2025-03-15 16:54:06 +0800

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,7 @@
-import { Fragment, ReactNode } from "react";
+import { ReactNode } from "react";
 import { Box, Text, useColorMode } from "@chakra-ui/react";
 import Hero from "./Hero";
-import Footer from "./Footer";
 import TopBar from "./TopBar";
-import Header from "./Header";
 
 type Props = {
   title?: string;
@@ -21,18 +19,13 @@ export default function Layout({
   children,
   title,
   subTitle,
-  pageTitle, 
-  pageMetaDescription, 
   withOutHero, 
-  useDefault = true,
   link,
   logoSrc,
 }: Props) {
   const { colorMode } = useColorMode();
 
-  return <Fragment>
-      <Header pageMetaDescription={pageMetaDescription} pageTitle={pageTitle} useDefault={useDefault}/>
-      <Box as="main" minHeight="100vh" mx={1} bg={colorMode === "dark" ? "gray.800" : "gray.50"} py={10}>
+  return <Box as="main" minHeight="100vh" mx={1} bg={colorMode === "dark" ? "gray.800" : "gray.50"} py={10}>
         <Box maxW={{ base: "xl", lg: "4xl" }} mx="auto" textAlign="center">
           <TopBar link={link} />
           {!withOutHero && <Hero title={title ?? "Plan Stacker"} withLogo height="20vh" logoSrc={logoSrc} />}
@@ -46,6 +39,4 @@ export default function Layout({
           </Box>
         </Box>
       </Box>
-      <Footer />
-    </Fragment>
 }
